caa1372e755031b6b5426c5a541cd5fcdd9e5be5
PigeonsHouse <bullbull1230@gmail.com>
2026-04-28 23:53:00 +0900

    update: TOPページ

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,18 @@
 const Index = () => {
   return (
-    <div>
-      <a href="/tsumikis">積み木一覧</a>
-      <br />
-      <a href="/works">作品一覧</a>
-    </div>
+    <>
+      <h1 style={{ textAlign: "center" }}>TOP</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: 16,
+        }}
+      >
+        <a href="/tsumikis">積み木一覧</a>
+        <a href="/works">作品一覧</a>
+      </div>
+    </>
   );
 };
 
